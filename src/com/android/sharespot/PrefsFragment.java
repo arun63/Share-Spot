7e3571f6724e960e3fdd09a28a96b4a75d1e7c03
arun <arun.in93@gmail.com>
2015-04-27 22:19:19 +0530

    Share-spot android application
    Android Application

---
+++ src/com/android/sharespot/PrefsFragment.java
@@ -0,0 +1,15 @@
+package com.android.sharespot;
+
+import com.android.sharespot.R;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class PrefsFragment extends PreferenceFragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.prefs);
+	}
+}
